5302fe62d151db54e7fb7dbce140897bd5930e02
Daisy0828 <56663292+Daisy0828@users.noreply.github.com>
2021-03-18 16:46:31 -0400

    Update auto_encoder.py

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import numpy as np
 from dtw import *
-# from tf_dtw import tf_dtw
+
+LAMBDA = 0.99
 
 class Encoder(tf.keras.Model):
 
     def __init__(self, input_shape, code_size, filters, kernel_sizes):
         super(Encoder, self).__init__()
         assert len(filters) == len(kernel_sizes)
-        assert len(input_shape) == 2 # (x, y), x = # of samples, y = # of vars
-        #self.input_shape = input_shape
+        assert len(input_shape) == 2  # (x, y), x = # of samples, y = # of vars
+        # self.input_shape = input_shape
         self.code_size = code_size
 
         self.convs = []
@@ -22,7 +23,7 @@ class Encoder(tf.keras.Model):
             b = tf.keras.layers.BatchNormalization()
             self.convs.append(l)
             self.norms.append(b)
-            output_len = output_len - (k-1)
+            output_len = output_len - (k - 1)
             output_channels = f
 
         self.last_kernel_shape = (output_len, output_channels)
@@ -37,11 +38,12 @@ class Encoder(tf.keras.Model):
             x = conv(x)
             x = norm(x, training=training)
         assert x.shape[1:] == self.last_kernel_shape
-        #print(x.shape)
+        # print(x.shape)
         x = self.flatten(x)
 
         x = self.out(x)
         return x
+
 
 class Decoder(tf.keras.Model):
 
@@ -49,7 +51,7 @@ class Decoder(tf.keras.Model):
         super(Decoder, self).__init__()
 
         assert len(last_kernel_shape) == 2
-        assert len(output_shape) == 2 # (x, y) x = # of samples, y = samples n variables
+        assert len(output_shape) == 2  # (x, y) x = # of samples, y = samples n variables
 
         self.code_size = code_size
         self.last_kernel_shape = last_kernel_shape
@@ -78,51 +80,53 @@ class Decoder(tf.keras.Model):
         assert self.expected_output_shape == x.shape[1:]
         return x
 
+
 _optimizer = tf.keras.optimizers.Nadam(learning_rate=0.00015)
 _mse_loss = tf.keras.losses.MeanSquaredError()
+
 
 class AutoEncoder:
     def __init__(self, **kwargs):
 
-        input_shape     = kwargs["input_shape"]
-        code_size       = kwargs["code_size"]
-        filters         = kwargs["filters"]
-        kernel_sizes    = kwargs["kernel_sizes"]
+        input_shape = kwargs["input_shape"]
+        code_size = kwargs["code_size"]
+        filters = kwargs["filters"]
+        kernel_sizes = kwargs["kernel_sizes"]
 
         if "loss" in kwargs:
-            loss        = kwargs["loss"]
+            loss = kwargs["loss"]
         else:
-            loss        = _mse_loss
+            loss = _mse_loss
 
         if "optimizer" in kwargs:
-            optimizer   = kwargs["optimizer"]
+            optimizer = kwargs["optimizer"]
         else:
-            optimizer   = _optimizer
+            optimizer = _optimizer
 
         self.encode = Encoder(input_shape, code_size, filters, kernel_sizes)
 
-        decoder_filters = list(filters[:len(filters)-1])
+        decoder_filters = list(filters[:len(filters) - 1])
         decoder_filters.append(input_shape[1])
         last_kernel_shape = self.encode.last_kernel_shape
 
         self.decode = Decoder(code_size, last_kernel_shape, input_shape, decoder_filters,
-                kernel_sizes)
+                              kernel_sizes)
 
         self.loss = loss
         self.optimizer = optimizer
 
+
 def similarity_funcs(input):
     # 50 x 150 x 1
     # 50 x 150
-    print("1")
     remove_last_dim = tf.reshape(input, [input.shape[0], input.shape[1]])
     # 1225 x 150
     total_distance = 0
     for i in range(remove_last_dim.shape[0]):
         for j in range(i + 1, remove_last_dim.shape[0]):
-            total_distance += tf_dtw(remove_last_dim[i], remove_last_dim[j])
+            total_distance += tf_dtw_with_matrix(tf.cast(remove_last_dim[i], dtype=tf.float64), tf.cast(remove_last_dim[j], dtype=tf.float64))
+            # dist = tf.linalg.norm(remove_last_dim[i] - remove_last_dim[j])
     return total_distance / (remove_last_dim.shape[0] * (remove_last_dim.shape[0] - 1) / 2)
-
 
 
 def eu_code_func(codes):
@@ -131,18 +135,16 @@ def eu_code_func(codes):
     total_distance = 0
     for i in range(remove_last_dim.shape[0]):
         for j in range(i + 1, remove_last_dim.shape[0]):
-            total_distance += tf.linalg.norm(remove_last_dim[i] - remove_last_dim[j])
+            dist = tf.linalg.norm(remove_last_dim[i] - remove_last_dim[j], axis=-1)
+            total_distance += dist
     return total_distance / (remove_last_dim.shape[0] * (remove_last_dim.shape[0] - 1) / 2)
 
 
 def tf_dtw(s, t):
     s = tf.cast(s, tf.float32)
     t = tf.cast(t, tf.float32)
-    print("sb")
     n = len(s)
     m = len(t)
-    # DTW = tf.fill([len(s)+1,len(t)+1], tf.tensor(np.inf))
-    # DTW = [[tf.Variable(np.inf) for _ in range(len(t)+1)] for _ in range(len(s)+1)]
     DTW = np.full([n + 1, m + 1], tf.Variable(np.inf))
     DTW[0][0].assign(0)
 
@@ -154,27 +156,145 @@ def tf_dtw(s, t):
     return DTW[-1][-1]
 
 
-@tf.function
-def train_step(input, auto_encoder, optimizer=_optimizer, loss = _mse_loss):
-    # print("!!!!!!!!!!!")
-    with tf.GradientTape() as tape:
-        # print("!!!!!!!!!!!")
+# def tf_dtw_with_matrix(s, t):
+#     # s = tf.convert_to_tensor([s,s,s], dtype = tf.float32)
+#     # t = tf.convert_to_tensor(np.transpose(np.array([t,t,t])), dtype = tf.float32)
+#     s = tf.convert_to_tensor(s, dtype=tf.float64)
+#     t = tf.convert_to_tensor(t, dtype=tf.float64)
+#
+#     x, y = tf.meshgrid(s, t)
+#
+#     distance_matrix = tf.math.subtract(y, x)
+#     distance_matrix = tf.math.abs(distance_matrix)
+#
+#     i = tf.Variable(len(distance_matrix) - 1)
+#     j = tf.Variable(len(distance_matrix[i]) - 1)
+#
+#     cost = tf.Variable(distance_matrix[i, j])
+#
+#     while i != 0 or j != 0:
+#         top = distance_matrix[i - 1, j] if i - 1 >= 0 else tf.constant(np.inf, dtype=tf.float64)
+#         left = distance_matrix[i, j - 1] if j - 1 >= 0 else tf.constant(np.inf, dtype=tf.float64)
+#         corner = distance_matrix[i - 1, j - 1] if j - 1 >= 0 and i - 1 >= 0 else tf.constant(np.inf, dtype=tf.float64)
+#
+#         next_val = tf.math.minimum(tf.math.minimum(top, left), corner)
+#
+#         if top == next_val:
+#             i.assign_sub(1)
+#         elif left == next_val:
+#             j.assign_sub(1)
+#         else:
+#             i.assign_sub(1)
+#             j.assign_sub(1)
+#
+#         cost.assign_add(distance_matrix[i, j])
+#
+#     return cost
+#
+# def tf_dtw_with_matrix(s, t):
+#     # s = tf.convert_to_tensor([s,s,s], dtype = tf.float32)
+#     # t = tf.convert_to_tensor(np.transpose(np.array([t,t,t])), dtype = tf.float32)
+#     s = tf.convert_to_tensor(s, dtype=tf.float64)
+#     t = tf.convert_to_tensor(t, dtype=tf.float64)
+#
+#     x, y = tf.meshgrid(s, t)
+#
+#     distance_matrix = tf.math.subtract(y, x)
+#     distance_matrix = tf.math.abs(distance_matrix)
+#
+#     i = tf.Variable(len(distance_matrix) - 1)
+#     j = tf.Variable(len(distance_matrix[i]) - 1)
+#
+#     cost = tf.Variable(distance_matrix[i, j])
+#
+#     while i != 0 or j != 0:
+#         top = distance_matrix[i - 1, j] if i - 1 >= 0 else tf.constant(np.inf)
+#         left = distance_matrix[i, j - 1] if j - 1 >= 0 else tf.constant(np.inf)
+#         corner = distance_matrix[i - 1, j - 1] if j - 1 >= 0 and i - 1 >= 0 else tf.constant(np.inf)
+#         print(top)
+#         print(left)
+#         print(corner)
+#
+#         if top == tf.math.minimum(tf.math.minimum(top, left), corner):
+#             i.assign_sub(1)
+#         elif left == tf.math.minimum(tf.math.minimum(top, left), corner):
+#             j.assign_sub(1)
+#         else:
+#             i.assign_sub(1)
+#             j.assign_sub(1)
+#
+#         cost.assign_add(distance_matrix[i, j])
+#
+#     return cost
 
+
+def tf_dtw_with_matrix(s, t):
+    # s = tf.convert_to_tensor([s,s,s], dtype = tf.float32)
+    # t = tf.convert_to_tensor(np.transpose(np.array([t,t,t])), dtype = tf.float32)
+    s = tf.convert_to_tensor(s, dtype=tf.float64)
+    t = tf.convert_to_tensor(t, dtype=tf.float64)
+
+    x, y = tf.meshgrid(s, t)
+
+    distance_matrix = tf.math.subtract(y, x)
+    distance_matrix = tf.math.abs(distance_matrix)
+
+    i = len(distance_matrix) - 1
+    j = len(distance_matrix[i]) - 1
+
+    cost = distance_matrix[i, j]
+
+    while i != 0 or j != 0:
+        top = distance_matrix[i - 1, j] if i - 1 >= 0 else np.inf
+        left = distance_matrix[i, j - 1] if j - 1 >= 0 else np.inf
+        corner = distance_matrix[i - 1, j - 1] if j - 1 >= 0 and i - 1 >= 0 else np.inf
+
+        if top == min(top, left, corner):
+            i -= 1
+        elif left == min(top, left, corner):
+            j -= 1
+        else:
+            i -= 1
+            j -= 1
+
+        cost += distance_matrix[i, j]
+
+    return cost
+
+def train_step(input, auto_encoder, optimizer=_optimizer, loss=_mse_loss):
+    with tf.GradientTape() as tape:
+        # # print(input.shape)
+        # dtw_input = similarity_funcs(input)
+        # # print(dtw_input)
+        # codes = auto_encoder.encode(input, training=True)
+        # eu_code = eu_code_func(codes)
+        # # print(eu_code)
+        # decodes = auto_encoder.decode(codes, training=True)
+        # reconstruction_loss = loss(input, decodes)
+        # # print(reconstruction_loss)
+        # similarity_loss = abs(tf.cast(eu_code, dtype=tf.float32) - tf.cast(dtw_input, dtype=tf.float32))
+        # # similarity_loss = loss(eu_code, dtw_input)
+        # # print(similarity_loss)
+        # loss = LAMBDA * reconstruction_loss + (1 - LAMBDA) * similarity_loss
+        # # loss = reconstruction_loss
+        # trainables = auto_encoder.encode.trainable_variables + auto_encoder.decode.trainable_variables
+
+        # print(input.shape)
         dtw_input = similarity_funcs(input)
-        # dtw_input = tf_dtw(input, len(input))
-        print(dtw_input)
+        # print(dtw_input)
         codes = auto_encoder.encode(input, training=True)
         eu_code = eu_code_func(codes)
         # print(eu_code)
         decodes = auto_encoder.decode(codes, training=True)
         reconstruction_loss = loss(input, decodes)
+        # dtw_input = similarity_funcs(decodes)
         # print(reconstruction_loss)
-        similarity_loss = loss(eu_code, dtw_input)
+        similarity_loss = abs(tf.cast(eu_code, dtype=tf.float32) - tf.cast(dtw_input, dtype=tf.float32))
+        # similarity_loss = loss(eu_code, dtw_input)
         # print(similarity_loss)
-        loss = (reconstruction_loss + similarity_loss) / 2
-        # loss = (reconstruction_loss + eu_code) / 2
+        loss = LAMBDA * reconstruction_loss + (1 - LAMBDA) * similarity_loss
+        # loss = reconstruction_loss
         trainables = auto_encoder.encode.trainable_variables + auto_encoder.decode.trainable_variables
     gradients = tape.gradient(loss, trainables)
     optimizer.apply_gradients(zip(gradients, trainables))
     return loss
-
